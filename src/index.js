5c461fbf92800a61873c4b4f2eefa529a684582f
Webs95 <alexandersemashko1995@yandex.ru>
2022-07-20 10:44:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = bracketsConfig.map((item) => item.join(''));
+
+  let bracketsStr = str.split('');
+  for (let i = 0; i < bracketsStr.length; i++) {
+    for (let a = 0; a < bracketsConfig.length; a++) {
+      if (
+        bracketsStr[i] === brackets[a][0] &&
+        bracketsStr[i + 1] === brackets[a][1]
+      ) {
+        bracketsStr.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+  return bracketsStr.length === 0 ? true : false;
+};
